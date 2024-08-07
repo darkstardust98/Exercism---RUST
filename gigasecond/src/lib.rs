eb469405d7d8a9f8854a5df17828ff478380266e
darkstardust98 <165183152+darkstardust98@users.noreply.github.com>
2024-08-07 16:15:21 +0200

    UPDATED problem 2-gigasecond with correct logic (way simpler than the method I was trying to make work yersterday)

--- gigasecond/src/lib.rs
+++ gigasecond/src/lib.rs
@@ -1,9 +1,12 @@
-use time::{Duration, OffsetDateTime, PrimitiveDateTime as DateTime, UtcOffset};
+use time::{Duration, PrimitiveDateTime as DateTime};
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    let start_date = start.assume_utc();
+    //let start_date = start.assume_utc();
     let duration = Duration::seconds(1_000_000_000);
-    let future_date = start_date + duration;
-    future_date.to_offset(UtcOffset::UTC).date().midnight()
+    let future_date = start + duration;
+    future_date
+    //println!("{}", future_date);
+    //println!("{}", future_date.to_offset(UtcOffset::UTC).date().midnight());
+   // future_date.to_offset(UtcOffset::UTC).date().midnight()
 }
