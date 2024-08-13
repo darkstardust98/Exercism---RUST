2bfe9f375e2096354684d68edfdf1b0135d3a629
darkstardust98 <165183152+darkstardust98@users.noreply.github.com>
2024-08-13 16:53:07 +0200

    UPDATED problem 2-clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,11 +1,21 @@
-pub struct Clock;
+pub struct Clock {
+    hours: i32,
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        let total_minutes = hours * 60 + minutes;
+        let normalized_hours = ((total_minutes / 60) % 24 + 24) % 24;
+        let normalized_minutes = ((total_minutes % 60) + 60) % 60;
+        Clock {
+            hours: normalized_hours,
+            minutes: normalized_minutes,
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        let total_minutes = self.hours * 60 + self.minutes + minutes;
+        Clock::new(0, total_minutes)
     }
 }
